2d8c84b9b4d438cf09718bbffd4a9fc3430241ec
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-09-21 17:26:58 +0500

    vector datastructure practice start today

---
+++ VectorDS.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+   
+   vector<int> firstVec={1,2,3,4,5};
+    
+    for(int i:firstVec){
+        cout<<i<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
