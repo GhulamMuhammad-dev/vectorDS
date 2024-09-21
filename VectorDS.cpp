82f160a828ad55950b5d5401c2b9e04b61a946a1
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-09-21 18:02:33 +0500

    The second commit of today

--- VectorDS.cpp
+++ VectorDS.cpp
@@ -6,14 +6,44 @@ using namespace std;
 int main(){
    
    vector<int> firstVec={1,2,3,4,5};
+   
     
     for(int i:firstVec){
         cout<<i<<endl;
     }
+   cout<<endl;
+    firstVec.push_back(10);
+
+    for(int i:firstVec){
+        cout<<i<<endl;
+    }
+
+cout<<endl;
+
+cout<<firstVec.capacity()<<endl;
+
+firstVec.pop_back();
+
+for(int i:firstVec){
+    cout<<i<<endl;
+}
 
 
+// linear search with vectors
 
+vector<string> fruits={"apple","mango","banana","pineapple","orange"} ;
 
+string target="coconut";
+
+for(string i:fruits){
+
+    if(i==target){
+        cout<<"Your fruit is found"<<endl;
+        break;
+    }
+    
+
+}
 
     return 0;
 }
